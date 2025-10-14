4c4d45c1667dd7110cee3020a7832bf23c3aa3c9
Abdullah24-it <693337@student.inholland.nl>
2025-10-14 12:55:06 +0200

    creat and read employee final

--- GardenGroupIncidentSystem/Repositories/IEmployeeRepository.cs
+++ GardenGroupIncidentSystem/Repositories/IEmployeeRepository.cs
@@ -3,36 +3,26 @@ using System.Collections.Generic;
 
 namespace GardenGroupIncidentSystem.Services.Repositories
 {
-    /// <summary>
-    /// Repository interface for Employee data access
-    /// Defines all CRUD operations for Employee entity
-    /// Author: [YOUR NAME]
-    /// </summary>
     public interface IEmployeeRepository
     {
-        // ID GENERATION
         string GetNextEmployeeId();
 
-        // CREATE
         Employee CreateEmployee(Employee employee);
         void CreateEmployees(List<Employee> employees);
 
-        // READ
         List<Employee> GetAllEmployees();
         Employee GetEmployeeById(string employeeId);
-        List<Employee> GetEmployeesByRole(string role);
+        List<Employee> GetEmployeesByRole(Role role);
         List<Employee> GetEmployeesByIds(List<string> employeeIds);
-        List<Employee> GetEmployeesByRoles(List<string> roles);
-        List<Employee> GetEmployeesByLocationAndRole(string location, string role);
+        List<Employee> GetEmployeesByRoles(List<Role> roles);
+        List<Employee> GetEmployeesByLocationAndRole(string location, Role role);
         List<Employee> SearchEmployeesByName(string searchTerm);
 
-        // HELPER METHODS
         bool EmployeeExists(string employeeId);
         long CountAllEmployees();
 
-        // AGGREGATION
         Dictionary<string, int> GetEmployeeCountByRole();
         Dictionary<string, int> GetEmployeeCountByLocation();
-        EmployeeStatistics GetEmployeeStatistics();
+        Employee.EmployeeStatistics GetEmployeeStatistics();
     }
 }
